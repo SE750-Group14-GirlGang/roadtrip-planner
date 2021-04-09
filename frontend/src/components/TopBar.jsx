342dc996491e975b58d42c061e2446da9cf2cac0
Courtney Hunter <courtney.hunter.nz@gmail.com>
2021-04-09 16:58:54 +1200

    TopBar component for dashboard

--- frontend/src/components/TopBar.jsx
+++ frontend/src/components/TopBar.jsx
@@ -1,5 +1,38 @@
-import { React } from "react";
+import React from 'react';
+import styles from '../styles/TopBar.module.css'
+import { Button, Typography } from "@material-ui/core";
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+// import * as PropTypes from "prop-types";
+
+// TODO Flags for host
+// class FlagsProvider extends React.Component {
+//   render() {
+//     return null;
+//   }
+// }
+//
+// FlagsProvider.propTypes = {
+//   features: PropTypes.shape({ moderate: PropTypes.bool }),
+//   children: PropTypes.node
+// };
 
 export default function TopBar() {
-  return <div />;
+  // <Grid container spacing={0} alignItems="center" className={styles.topBar}>
+  return (
+    <div className={styles.topBar}>
+      <Typography variant="h2" className={styles.title}>
+        Roadie
+      </Typography>
+      {/*<h1>Roadie</h1>*/}
+      <div className={styles.navItems}>
+        <Button>Join Existing Group</Button>
+        <Button>Create New Group</Button>
+        {/*TODO Add person to group only if HOST and in group, not dashboard*/}
+        {/*<FlagsProvider features={{ moderate: user.role === "admin" }}>*/}
+        {/*  <Button>Host Only</Button>*/}
+        {/*</FlagsProvider>*/}
+        <Button><AccountCircleIcon/></Button>
+      </div>
+    </div>
+  );
 }
