c95b207253373f880d691f5f9c8b8fef0fa1f15f
Courtney Hunter <courtney.hunter.nz@gmail.com>
2021-04-09 14:41:03 +1200

    Configure dashboard page

--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -1,7 +1,15 @@
-import { React } from 'react';
+import React from 'react';
+import styles from '../styles/Dashboard.module.css'
+import TopBar from "../components/TopBar";
+import MyGroups from "../components/dashboard/MyGroups";
+import PendingGroups from "../components/dashboard/PendingGroups";
 
 export default function Dashboard() {
     return (
-        <div/>
+      <div className={styles.dashboard}>
+          <TopBar />
+          <MyGroups />
+          <PendingGroups />
+      </div>
     );
 }
