be222ddac2de880b8b82a3929b81362d3c0e02ff
Denise PC <43567020+Denijar@users.noreply.github.com>
2021-04-09 21:30:03 +1200

    adds roadtrips to the user schema

--- backend/src/db/models/User.js
+++ backend/src/db/models/User.js
@@ -6,6 +6,14 @@ const userSchema = new Schema({
     email: { type: String, unique: true, required: true },
     firstName: String,
     lastName: String,
+    roadTripsOrganising: { 
+        type: [{ type: Schema.Types.ObjectId, ref: 'RoadTrip' }], 
+        default: [] 
+    }, 
+    roadTripsAttending: { 
+        type: [{ type: Schema.Types.ObjectId, ref: 'RoadTrip' }], 
+        default: [] 
+    }
 }, { collection: 'users' });
 
 export const User = mongoose.model('User', userSchema);
