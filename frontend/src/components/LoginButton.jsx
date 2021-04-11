52738002e4056052f51189415f6420915c99e675
Jennak21 <12jenna.k@gmail>
2021-04-11 12:51:22 +1200

    styling

--- frontend/src/components/LoginButton.jsx
+++ frontend/src/components/LoginButton.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import styles from "../styles/LoginButton.module.css";
 
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
   return (
     <button
+      className ={styles.button}
       onClick={() => loginWithRedirect()}
     >
       Log In
