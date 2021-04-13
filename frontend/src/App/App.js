874458b7b3a4227df6e897d82595838301596774
Jennak21 <12jenna.k@gmail>
2021-04-13 14:47:06 +1200

    fixed routing and implemented loading div

--- frontend/src/App/App.js
+++ frontend/src/App/App.js
@@ -1,54 +1,18 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Redirect,
-  Route,
-  Switch,
-} from "react-router-dom";
+import { Switch } from "react-router-dom";
 import RoadTripPage from "../pages/RoadTripPage/RoadTripPage";
 import DashboardPage from "../pages/DashboardPage/DashboardPage";
-import HomePage from "../pages/HomePage/HomePage";
-import { useAuth0 } from "@auth0/auth0-react";
 import ProtectedRoute from "../auth/protected-route";
 import styles from "./App.module.css";
 
 function App() {
-  const { isAuthenticated } = useAuth0();
-
-  // const PublicRoute = ({ auth, ...props }) => {
-  //   return isAuthenticated ? (
-  //     <Route to="/">
-  //       <Dashboard />
-  //     </Route>
-  //   ) : (
-  //     <Route {...props} />
-  //   );
-  // };
-
-  // const ProtectedRoute = ({ auth, ...props }) => {
-  //   console.log(isAuthenticated);
-  //   return isAuthenticated ? (
-  //     <Route {...props} />
-  //   ) : (
-  //     <Route to="/home">
-  //       <HomePage />
-  //     </Route>
-  //   );
-  // };
-
   return (
-    <Router>
-      <div className={styles.App}>
-        <Switch>
-          <Route exact path="/">
-            <DashboardPage />
-          </Route>
-          <Route path="/road-trip/:id">
-            <RoadTripPage />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <div className={styles.App}>
+      <Switch>
+        <ProtectedRoute exact path="/" component={DashboardPage} />
+        <ProtectedRoute path="/road-trip/:id" component={RoadTripPage} />
+      </Switch>
+    </div>
   );
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {
+  BrowserRouter as Router,
+} from "react-router-dom";
 import "./index.css";
 import App from "./App/App";
 import reportWebVitals from "./reportWebVitals";
@@ -7,9 +10,11 @@ import Auth0ProviderWithHistory from "./auth/auth0-provider-with-history";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Auth0ProviderWithHistory>
-      <App />
-    </Auth0ProviderWithHistory>
+    <Router>
+      <Auth0ProviderWithHistory>
+        <App />
+      </Auth0ProviderWithHistory>
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
