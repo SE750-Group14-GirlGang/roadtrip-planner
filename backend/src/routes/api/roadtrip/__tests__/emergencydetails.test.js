b174fd417935f54a745996e1c3f3efbc0efa74d8
Denise PC <43567020+Denijar@users.noreply.github.com>
2021-04-21 16:12:08 +1200

    adds test for itinerary endpoint

--- backend/src/routes/api/roadtrip/__tests__/emergencydetails.test.js
+++ backend/src/routes/api/roadtrip/__tests__/emergencydetails.test.js
@@ -6,7 +6,7 @@ import axios from 'axios';
 
 let mongod, app, server;
 
-let roadTrip, emergencyDetails1;
+let roadTrip, emergencyDetails;
 
 beforeAll(async done => {
 
@@ -25,7 +25,7 @@ beforeEach(async () => {
     const roadTripsColl = await mongoose.connection.db.createCollection('roadtrips');
     const emergencyDetailsColl = await mongoose.connection.db.createCollection('emergencydetails');
 
-    emergencyDetails1 = {
+    emergencyDetails = {
         user: 1,
         phoneNumber: '0000000',
         emergencyContact: {
@@ -36,13 +36,13 @@ beforeEach(async () => {
         allergies: [ 'nuts' ],
         medicalConditions: [ 'hemophilia' ]
     };
-    await emergencyDetailsColl.insertOne(emergencyDetails1);
+    await emergencyDetailsColl.insertOne(emergencyDetails);
 
     roadTrip = {
         organiser: 0,
         name: 'My Road Trip',
         emergencyDetails: [
-            emergencyDetails1._id
+            emergencyDetails._id
         ]
     };
     await roadTripsColl.insertOne(roadTrip);
@@ -65,15 +65,16 @@ afterAll(done => {
 it('gets emergency details for a roadtrip from the server', async () => {
 
     const response = await axios.get(`http://localhost:3000/api/roadtrip/${roadTrip._id}/emergencydetails`);
-    const emergencyDetails = response.data;
+    const emergencyDetailsRes = response.data;
 
-    expect(emergencyDetails).toBeTruthy();
-    expect(emergencyDetails.length).toBe(1);
+    expect(emergencyDetailsRes).toBeTruthy();
+    expect(emergencyDetailsRes.length).toBe(1);
 
-    expect(emergencyDetails[0].phoneNumber).toBe('0000000');
-    expect(emergencyDetails[0].emergencyContact.name).toBe('Peter');
+    expect(emergencyDetailsRes[0].phoneNumber).toBe('0000000');
+    expect(emergencyDetailsRes[0].emergencyContact.name).toBe('Peter');
 });
 
+// Test times out :( want to ask Andrew for guidance on how to test a POST
 // it('posts emergency details for a roadtrip to the server and then can get', async () => {
 
 //     const postReponse = await axios.post(`http://localhost:3000/api/roadtrip/${roadTrip._id}/emergencydetails`, {
---
+++ backend/src/routes/api/roadtrip/__tests__/itinerary.test.js
@@ -0,0 +1,73 @@
+import router from '../../../index';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import express from 'express';
+import axios from 'axios';
+
+let mongod, app, server;
+
+let roadTrip, itinerary;
+
+beforeAll(async done => {
+
+    mongod = new MongoMemoryServer();
+
+    const connectionString = await mongod.getUri();
+    await mongoose.connect(connectionString, { useNewUrlParser: true });
+
+    app = express();
+    app.use('/', router);
+    server = app.listen(3000, () => done());
+
+});
+
+beforeEach(async () => {
+    const roadTripsColl = await mongoose.connection.db.createCollection('roadtrips');
+    const itineraryColl = await mongoose.connection.db.createCollection('itineraries');
+
+    const april_21_2021 = new Date(2021,3,21); // months are indexed from 0
+
+    itinerary = {
+        dates: [april_21_2021],
+        itineraryDays: [{
+            day: april_21_2021,
+            time: "Morning",
+            location: "Cathedral Cove",
+            notes: "Bring your togs!"
+        }]
+    };
+    await itineraryColl.insertOne(itinerary);
+
+    roadTrip = {
+        organiser: 0,
+        name: 'My Road Trip',
+        itinerary: itinerary._id
+    };
+    await roadTripsColl.insertOne(roadTrip);
+});
+
+afterEach(async () => {
+    await mongoose.connection.db.dropCollection('roadtrips');
+    await mongoose.connection.db.dropCollection('itineraries');
+});
+
+afterAll(done => {
+    server.close(async () => {
+        await mongoose.disconnect();
+        await mongod.stop();
+
+        done();
+    });
+});
+
+it('gets itinerary for a roadtrip from the server', async () => {
+    const response = await axios.get(`http://localhost:3000/api/roadtrip/${roadTrip._id}/itinerary`);
+    const itineraryRes = response.data;
+
+    expect(itineraryRes).toBeTruthy();
+
+    expect(itineraryRes.dates.length).toBe(1);
+    expect(itineraryRes.itineraryDays[0].time).toBe('Morning');
+    expect(itineraryRes.itineraryDays[0].location).toBe('Cathedral Cove');
+    expect(itineraryRes.itineraryDays[0].notes).toBe('Bring your togs!');
+});
