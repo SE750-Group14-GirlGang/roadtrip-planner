19381f20e37313d009aeb9eea7885fad2becd242
Denise PC <43567020+Denijar@users.noreply.github.com>
2021-04-21 16:15:28 +1200

    adds test for map endpoint

---
+++ backend/src/routes/api/roadtrip/__tests__/map.test.js
@@ -0,0 +1,70 @@
+import router from '../../../index';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import express from 'express';
+import axios from 'axios';
+
+let mongod, app, server;
+
+let roadTrip, map;
+
+beforeAll(async done => {
+
+    mongod = new MongoMemoryServer();
+
+    const connectionString = await mongod.getUri();
+    await mongoose.connect(connectionString, { useNewUrlParser: true });
+
+    app = express();
+    app.use('/', router);
+    server = app.listen(3000, () => done());
+
+});
+
+beforeEach(async () => {
+    const roadTripsColl = await mongoose.connection.db.createCollection('roadtrips');
+    const mapColl = await mongoose.connection.db.createCollection('maps');
+
+    map = {
+        primaryDestination: {
+            number: "123",
+            street: "Queen Street",
+            city: "Auckland",
+            postcode: "1010"
+        }
+    };
+    await mapColl.insertOne(map);
+
+    roadTrip = {
+        organiser: 0,
+        name: 'My Road Trip',
+        map: map._id
+    };
+    await roadTripsColl.insertOne(roadTrip);
+});
+
+afterEach(async () => {
+    await mongoose.connection.db.dropCollection('roadtrips');
+    await mongoose.connection.db.dropCollection('maps');
+});
+
+afterAll(done => {
+    server.close(async () => {
+        await mongoose.disconnect();
+        await mongod.stop();
+
+        done();
+    });
+});
+
+it('gets map for a roadtrip from the server', async () => {
+    const response = await axios.get(`http://localhost:3000/api/roadtrip/${roadTrip._id}/map`);
+    const mapRes = response.data;
+
+    expect(mapRes).toBeTruthy();
+
+    expect(mapRes.primaryDestination.number).toBe("123");
+    expect(mapRes.primaryDestination.street).toBe("Queen Street");
+    expect(mapRes.primaryDestination.city).toBe("Auckland");
+    expect(mapRes.primaryDestination.postcode).toBe("1010");
+});
