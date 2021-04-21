55e9034b996248f19f0a3aada4677ce2e65373af
Denise PC <43567020+Denijar@users.noreply.github.com>
2021-04-21 15:27:44 +1200

    adds passing tests for roadtrip and emergencydetails endpoints

---
+++ backend/src/routes/api/roadtrip/__tests__/emergencydetails.test.js
@@ -0,0 +1,103 @@
+import router from '../../../index';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import express from 'express';
+import axios from 'axios';
+
+let mongod, app, server;
+
+let roadTrip, emergencyDetails1;
+
+beforeAll(async done => {
+
+    mongod = new MongoMemoryServer();
+
+    const connectionString = await mongod.getUri();
+    await mongoose.connect(connectionString, { useNewUrlParser: true });
+
+    app = express();
+    app.use('/', router);
+    server = app.listen(3000, () => done());
+
+});
+
+beforeEach(async () => {
+    const roadTripsColl = await mongoose.connection.db.createCollection('roadtrips');
+    const emergencyDetailsColl = await mongoose.connection.db.createCollection('emergencydetails');
+
+    emergencyDetails1 = {
+        user: 1,
+        phoneNumber: '0000000',
+        emergencyContact: {
+            name: 'Peter',
+            phoneNumber: '1111111',
+            relation: 'Father'
+        },
+        allergies: [ 'nuts' ],
+        medicalConditions: [ 'hemophilia' ]
+    };
+    await emergencyDetailsColl.insertOne(emergencyDetails1);
+
+    roadTrip = {
+        organiser: 0,
+        name: 'My Road Trip',
+        emergencyDetails: [
+            emergencyDetails1._id
+        ]
+    };
+    await roadTripsColl.insertOne(roadTrip);
+});
+
+afterEach(async () => {
+    await mongoose.connection.db.dropCollection('roadtrips');
+    await mongoose.connection.db.dropCollection('emergencydetails');
+});
+
+afterAll(done => {
+    server.close(async () => {
+        await mongoose.disconnect();
+        await mongod.stop();
+
+        done();
+    });
+});
+
+it('gets emergency details for a roadtrip from the server', async () => {
+
+    const response = await axios.get(`http://localhost:3000/api/roadtrip/${roadTrip._id}/emergencydetails`);
+    const emergencyDetails = response.data;
+
+    expect(emergencyDetails).toBeTruthy();
+    expect(emergencyDetails.length).toBe(1);
+
+    expect(emergencyDetails[0].phoneNumber).toBe('0000000');
+    expect(emergencyDetails[0].emergencyContact.name).toBe('Peter');
+});
+
+// it('posts emergency details for a roadtrip to the server and then can get', async () => {
+
+//     const postReponse = await axios.post(`http://localhost:3000/api/roadtrip/${roadTrip._id}/emergencydetails`, {
+//         user: 2,
+//         phoneNumber: '1234567',
+//         emergencyContact: {
+//             name: 'Susan',
+//             phoneNumber: '7654321',
+//             relation: 'Father'
+//         },
+//         allergies: [ 'milk' ],
+//         medicalConditions: []
+//     });
+
+//     const emergencyDetailsLocation = postReponse.headers.Location;
+//     const getResponse = await axios.get(emergencyDetailsLocation);
+//     const emergencyDetails = getResponse.data;
+
+//     expect(emergencyDetails).toBeTruthy();
+//     expect(emergencyDetails.length).toBe(2);
+
+//     expect(emergencyDetails[0].phoneNumber).toBe('0000000');
+//     expect(emergencyDetails[0].emergencyContact.name).toBe('Peter');
+
+//     expect(emergencyDetails[1].phoneNumber).toBe('1234567');
+//     expect(emergencyDetails[1].emergencyContact.name).toBe('Susan');
+// });
---
+++ backend/src/routes/api/roadtrip/__tests__/roadtrip.test.js
@@ -0,0 +1,70 @@
+import router from '../../../index';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import express from 'express';
+import axios from 'axios';
+
+let mongod, app, server;
+let roadTrip1, roadTrip2;
+
+beforeAll(async done => {
+
+    mongod = new MongoMemoryServer();
+
+    const connectionString = await mongod.getUri();
+    await mongoose.connect(connectionString, { useNewUrlParser: true });
+
+    app = express();
+    app.use('/', router);
+    server = app.listen(3000, () => done());
+
+});
+
+beforeEach(async () => {
+    const roadTripsColl = await mongoose.connection.db.createCollection('roadtrips');
+
+    roadTrip1 = {
+        organiser: 0,
+        name: 'The First Roadtrip'
+    };
+    roadTrip2 = {
+        organiser: 0,
+        name: 'The Second Roadtrip'
+    };
+    await roadTripsColl.insertMany([roadTrip1, roadTrip2]);
+});
+
+afterEach(async () => {
+    await mongoose.connection.db.dropCollection('roadtrips');
+});
+
+afterAll(done => {
+    server.close(async () => {
+        await mongoose.disconnect();
+        await mongod.stop();
+
+        done();
+    });
+});
+
+it('gets all roadtrips', async () => {
+
+    const response = await axios.get(`http://localhost:3000/api/roadtrip`);
+    const roadTrips = response.data;
+
+    expect(roadTrips).toBeTruthy();
+    expect(roadTrips.length).toBe(2);
+
+    expect(roadTrips[0].name).toBe('The First Roadtrip');
+    expect(roadTrips[1].name).toBe('The Second Roadtrip');
+});
+
+it('gets a single roadtrip', async () => {
+
+    const response = await axios.get(`http://localhost:3000/api/roadtrip/${roadTrip2._id}`);
+    const roadTrip = response.data;
+
+    expect(roadTrip).toBeTruthy();
+
+    expect(roadTrip.name).toBe('The Second Roadtrip');
+});
