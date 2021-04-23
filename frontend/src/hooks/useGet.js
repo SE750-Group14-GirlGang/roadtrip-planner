bbc639f4d8cd29f67c92ca7e9eb4757b12752920
Jennak21 <12jenna.k@gmail>
2021-04-23 14:35:03 +1200

    Get access token from auth0 and send it in authorization headers for API requests

--- frontend/src/hooks/useGet.js
+++ frontend/src/hooks/useGet.js
@@ -1,15 +1,24 @@
 import { useEffect, useState } from "react";
 import axios from 'axios';
+import { useAuth0 } from '@auth0/auth0-react';
 
 export default function useGet(url, config = {}) {
     
     const [response, setResponse] = useState(null);
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(false);
+    const { getAccessTokenSilently } = useAuth0();
 
     useEffect(() => {
         async function performGet() {
             setLoading(true);
+
+            const accessToken = await getAccessTokenSilently();
+            // set token in Authorization header
+            config.headers = {
+                Authorization: `Bearer ${accessToken}`,
+            }
+
             try {
                 const axiosResponse = await axios.get(url, config);
                 setResponse(axiosResponse);
