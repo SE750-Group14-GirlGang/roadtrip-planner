8777effb52ba6c190b2493a45b806f6229e62c22
Laura Huijser <laura.huijser@yahoo.com>
2021-04-24 11:20:53 +1200

    Issue 20 - Add base empty map page state

--- frontend/src/pages/RoadTripPage/MapPage/MapPage.jsx
+++ frontend/src/pages/RoadTripPage/MapPage/MapPage.jsx
@@ -1,6 +1,8 @@
 import { React } from "react";
 import ReactMapGL from "react-map-gl";
 import TopBar from "../../../components/TopBar/TopBar";
+import { Button } from "@material-ui/core";
+import styles from "./MapPage.module.css";
 
 const dotenv = require("dotenv");
 dotenv.config();
@@ -21,12 +23,20 @@ export default function MapPage() {
   return (
     <div>
       <TopBar />
-      {mapPageSetUp && (
+      <h3>Destination</h3>
+      {mapPageSetUp ? (
         <ReactMapGL
           {...state.viewport}
           mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
           mapStyle="mapbox://styles/lhuijser/ckntkputc066c17lz7nby5fxs"
         ></ReactMapGL>
+      ) : (
+        <div className={styles.mapPage}>
+          <br />
+          <h4>The host has not entered a destination yet!</h4>
+          <br />
+          <Button>Create New Group</Button>
+        </div>
       )}
     </div>
   );
