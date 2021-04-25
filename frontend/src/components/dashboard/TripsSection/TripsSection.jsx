2819b3bb4624f2c10e44b17194d86d5d25a76db3
Denise PC <43567020+Denijar@users.noreply.github.com>
2021-04-25 17:17:55 +1200

    moves custom spinner into separate file

--- frontend/src/components/dashboard/TripsSection/TripsSection.jsx
+++ frontend/src/components/dashboard/TripsSection/TripsSection.jsx
@@ -1,13 +1,7 @@
 import React from "react";
-import { CircularProgress, withStyles } from "@material-ui/core";
 import styles from "./TripsSection.module.css";
+import { CustomCircularProgress } from "./TripsSection.styles";
 import TripsList from "../commons/TripsList/TripsList";
-
-const CustomCircularProgress = withStyles(() => ({
-	colorPrimary: {
-	  color: "#f76c6c",
-	},
-  }))(CircularProgress);
 
 export default function GroupsSection({ title, trips, loading, disable }) {
     return (
