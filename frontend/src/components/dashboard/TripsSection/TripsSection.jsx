88a328a751e40a04a4d22780ff4a33693c6d16e2
Denise PC <43567020+Denijar@users.noreply.github.com>
2021-04-25 16:38:00 +1200

    adds a loading spinner

--- frontend/src/components/dashboard/TripsSection/TripsSection.jsx
+++ frontend/src/components/dashboard/TripsSection/TripsSection.jsx
@@ -1,12 +1,19 @@
 import React from "react";
+import { CircularProgress, withStyles } from "@material-ui/core";
 import styles from "./TripsSection.module.css";
 import TripsList from "../commons/TripsList/TripsList";
+
+const CustomCircularProgress = withStyles(() => ({
+	colorPrimary: {
+	  color: "#f76c6c",
+	},
+  }))(CircularProgress);
 
 export default function GroupsSection({ title, trips, loading, disable }) {
     return (
         <div className={styles.section}>
             <h1>{title}</h1>
-            {loading && <div>...fetching trips</div>}
+            {loading && <CustomCircularProgress />}
             {trips && <TripsList trips={trips} disable={disable} />}
         </div>
     );
