d24294be3c4e69ff6f96a96826987bb3ae5588b3
Jennak21 <12jenna.k@gmail>
2021-04-27 12:35:20 +1200

    code styling

--- frontend/src/utils/dates/getDaysInbetween.js
+++ frontend/src/utils/dates/getDaysInbetween.js
@@ -1,8 +1,11 @@
 export default function getDaysInbetween(start, end) {
-    let startDate = new Date(start)
-    for(var arr=[]; startDate<=end; startDate.setDate(startDate.getDate()+1)){
+    let startDate = new Date(start);
+    for (
+        var arr = [];
+        startDate <= end;
+        startDate.setDate(startDate.getDate() + 1)
+    ) {
         arr.push(new Date(startDate));
     }
     return arr;
-
 }
