30d9646f88b922c8893e4ef5091ef20cb554c536
Denijar <denijar@gmail.com>
2021-04-29 11:35:01 +1200

    post half implemented - need merge with usePost hook fix

--- frontend/src/components/TopBar/CreateTripModal/CreateTripModal.jsx
+++ frontend/src/components/TopBar/CreateTripModal/CreateTripModal.jsx
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { useAuth0 } from "@auth0/auth0-react";
 import Button from "@material-ui/core/Button";
 import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
@@ -7,18 +10,45 @@ import DialogTitle from "@material-ui/core/DialogTitle";
 import TextField from "@material-ui/core/TextField";
 
 export default function CreateTripModal({ open, handleClose }) {
+  const { getAccessTokenSilently } = useAuth0();
+  const { id } = useParams();
+
+  const [name, setName] = useState("untitled road trip");
+
+  const handleChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleSubmit = async () => {
+    const accessToken = await getAccessTokenSilently();
+    const config = {
+        headers: {
+            Authorization: `Bearer ${accessToken}`,
+        },
+    };
+    const roadTripToPost = {
+        name
+    };
+    const roadTrip = await axios.post(
+        `/api/roadtrip/${id}`,
+        roadTripToPost,
+        config
+    );
+    handleClose();
+  }
+
   return (
     <div>
       <Dialog fullWidth={true} open={open} onClose={handleClose}>
-        <DialogTitle>Create New Trip</DialogTitle>
+        <DialogTitle>Create new trip</DialogTitle>
         <DialogContent>
-          <TextField label="Road Trip Name" variant="outlined" />
+          <TextField fullWidth autoFocus label="Enter name" defaultValue={name} onChange={handleChange} />
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose} color="primary">
             Cancel
           </Button>
-          <Button onClick={handleClose} color="primary">
+          <Button onClick={handleSubmit} color="primary" disabled={!name}>
             Submit
           </Button>
         </DialogActions>
