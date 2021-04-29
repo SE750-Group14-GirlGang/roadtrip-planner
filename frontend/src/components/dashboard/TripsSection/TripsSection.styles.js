7c462ca80c1ac08fb70ce3156179ff0cff56bfc4
Denijar <denijar@gmail.com>
2021-04-29 14:42:07 +1200

    deletes now unused file

--- frontend/src/components/dashboard/TripsSection/TripsSection.styles.js
+++
@@ -1,9 +0,0 @@
-import { CircularProgress, withStyles } from '@material-ui/core';
-
-const CustomCircularProgress = withStyles(() => ({
-  colorPrimary: {
-    color: '#f76c6c',
-  },
-}))(CircularProgress);
-
-export default CustomCircularProgress;
