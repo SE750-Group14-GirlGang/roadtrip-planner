c51f7caa4b44fb9d77a6e89c720eb91a8f0b6fde
Laura Huijser <laura.huijser@yahoo.com>
2021-05-01 10:57:01 +1200

    Change attendees button depending on if organiser or not

--- frontend/src/components/RoadTripTopBar/RoadTripTopBar.jsx
+++ frontend/src/components/RoadTripTopBar/RoadTripTopBar.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { Button, AppBar, Toolbar, withStyles, makeStyles } from '@material-ui/core';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import GroupRoundedIcon from '@material-ui/icons/GroupRounded';
 import GroupAddRoundedIcon from '@material-ui/icons/GroupAddRounded';
 import HomeRoundedIcon from '@material-ui/icons/HomeRounded';
 import { NavLink, useParams } from 'react-router-dom';
@@ -8,6 +9,7 @@ import SideBar from '../SideBar/SideBar';
 import styles from './RoadTripTopBar.module.css';
 import useGet from '../../hooks/useGet';
 import AttendeesModal from './AttendeesModal/AttendeesModal';
+import { OrganiserContext } from '../../contexts/OrganiserContextProvider';
 
 const useStyles = makeStyles(() => ({
   grow: {
@@ -33,6 +35,7 @@ const CustomTopBar = withStyles({
 export default function RoadTripTopBar() {
   const { id } = useParams();
   const { response } = useGet(`/api/roadtrip/${id}`);
+  const { isUserOrganiser } = useContext(OrganiserContext);
   const classes = useStyles();
 
   const [modalOpen, setModalOpen] = useState(false);
@@ -52,7 +55,11 @@ export default function RoadTripTopBar() {
         <p className={styles.title}>{response?.data.name}</p>
         <div className={classes.grow} />
         <IconButton>
-          <GroupAddRoundedIcon onClick={handleOpenModal} />
+          {isUserOrganiser ? (
+            <GroupAddRoundedIcon onClick={handleOpenModal} />
+          ) : (
+            <GroupRoundedIcon onClick={handleOpenModal} />
+          )}
         </IconButton>
         <NavLink to="/">
           <IconButton>
