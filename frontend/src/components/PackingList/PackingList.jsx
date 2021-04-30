bcf3a2cec8c6fe74ed7e014f187e44a92a60b711
Denijar <denijar@gmail.com>
2021-04-30 17:16:02 +1200

    actually commit changes to PackingList.jsx

--- frontend/src/components/PackingList/PackingList.jsx
+++ frontend/src/components/PackingList/PackingList.jsx
@@ -11,19 +11,19 @@ export default function PackingList() {
   const classes = useStyles();
   // TODO: implement full packing list functionality with link to backend
   // const packingListItems = [
-  //     "sleeping bag",
-  //     "sleeping mat",
-  //     "rain jacket",
-  //     "pillow",
-  //     "alcohol",
-  //     "clothing",
-  //     "cutlery",
-  //     "towel",
-  //     "togs",
-  //     "phone charger",
-  //     "ear plugs",
-  //     "hoons",
-  //     "bowl",
+  //   'sleeping bag',
+  //   'sleeping mat',
+  //   'rain jacket',
+  //   'pillow',
+  //   'alcohol',
+  //   'clothing',
+  //   'cutlery',
+  //   'towel',
+  //   'togs',
+  //   'phone charger',
+  //   'ear plugs',
+  //   'hoons',
+  //   'bowl',
   // ];
   const packingListItems = [];
   const [userPackedItems, setUserPackedItems] = useState(['sleeping bag', 'hoons', 'alcohol', 'ear plugs']);
@@ -40,27 +40,24 @@ export default function PackingList() {
 
   return (
     <div className={styles.container}>
-      <div className={styles.card}>
-        <div className={styles.cardHeader}>Packing List</div>
+      {packingListItems.length ? (
         <div className={styles.cardContent}>
-          {packingListItems.length ? (
-            packingListItems.map((item, index) => (
-              <div key={index}>
-                <Item
-                  name={item}
-                  checked={userPackedItems.includes(item)}
-                  // TODO: if time - convert this list to a map for speed
-                  onChange={() => handleChange(item)}
-                />
-              </div>
-            ))
-          ) : (
-            <div className={styles.emptyText}> The organiser has not added any items to the packing list!</div>
-          )}
+          {packingListItems.map((item, index) => (
+            <div key={index}>
+              <Item
+                name={item}
+                checked={userPackedItems.includes(item)}
+                // TODO: if time - convert this list to a map for speed
+                onChange={() => handleChange(item)}
+              />
+            </div>
+          ))}
         </div>
-        <div className={styles.cardFooter}>
-          {userIsOrganiser && <Button className={classes.button}>+ Add Item</Button>}
-        </div>
+      ) : (
+        <div className={styles.emptyText}> The organiser has not added any items to the packing list!</div>
+      )}
+      <div className={styles.buttonContainer}>
+        {userIsOrganiser && <Button className={classes.button}>Add Item</Button>}
       </div>
     </div>
   );
