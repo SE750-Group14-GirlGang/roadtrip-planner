0fe934f33db4c2d4e85daa44b50baa6568560281
Courtney Hunter <courtney.hunter.nz@gmail.com>
2021-05-02 21:57:28 +1200

    Get access and refresh tokens

--- frontend/src/components/playlist/InitialisePlaylist/InitialisePlaylist.jsx
+++ frontend/src/components/playlist/InitialisePlaylist/InitialisePlaylist.jsx
@@ -1,11 +1,39 @@
 import { Button } from '@material-ui/core';
 import React from 'react';
-import axios from 'axios';
 
 const SPOTIFY_BASE = 'https://accounts.spotify.com';
 const redirect_uri = 'http://localhost:3000/road-trip/608a11a9b1a2ffd010b1d738/spotify-playlist/callback';
 const client_id = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
 const client_secret = process.env.REACT_APP_SPOTIFY_CLIENT_SECRET;
+
+const request = require('request'); // "Request" library
+
+const getToken = (authorization_code) => {
+  const authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    form: {
+      code: authorization_code,
+      redirect_uri,
+      grant_type: 'authorization_code',
+    },
+    headers: {
+      Authorization: `Basic ${Buffer.from(`${client_id}:${client_secret}`).toString('base64')}`,
+    },
+    json: true,
+  };
+
+  request.post(authOptions, function (error, response, body) {
+    console.log(body);
+
+    if (!error && response.statusCode === 200) {
+      console.log('Success!');
+      console.log(response);
+    } else {
+      console.log('Bad!');
+      console.log(response);
+    }
+  });
+};
 
 function authorizeSpotify() {
   const scopes = 'user-read-private user-read-email';
@@ -19,37 +47,12 @@ function authorizeSpotify() {
   window.location.replace(auth_url);
 }
 
-// TODO Fix Token 400 response
-const getToken = (authorization_code) => {
-  return axios({
-    method: 'POST',
-    url: 'https://accounts.spotify.com/api/token',
-    params: {
-      code: authorization_code,
-      grant_type: 'authorization_code',
-      redirect_uri,
-    },
-    headers: {
-      Authorization: `Basic ${Buffer.from(`${client_id}:${client_secret}`).toString('base64')}`,
-      'Content-Type': 'application/x-www-form-urlencoded',
-    },
-  })
-    .then((token) => {
-      return token;
-    })
-    .catch((error) => {
-      console.error(error.response);
-      console.error(error.response.data);
-    });
-};
-
 export default function InitialisePlaylist() {
   const urlParams = new URLSearchParams(window.location.search);
   const authorization_code = urlParams.get('code');
 
   if (authorization_code) {
-    const token = getToken(authorization_code);
-    return <div>Trying to get Token</div>;
+    return <Button onClick={() => getToken(authorization_code)}>Get Token</Button>;
   }
 
   return <Button onClick={authorizeSpotify}>Log In to Spotify</Button>;
