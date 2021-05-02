c1f27ee567279cd5553a6f9a45bb9ac3396e1a10
Denijar <denijar@gmail.com>
2021-05-02 22:32:55 +1200

    implement user specific end points for packeditems

--- backend/src/db/controllers/packeditems.js
+++ backend/src/db/controllers/packeditems.js
@@ -3,19 +3,33 @@ import { RoadTrip } from '../models/RoadTrip';
 
 import { getRoadTrip } from './roadtrips';
 
-export async function getPackedItems(roadTripId) {
+export async function getAllPackedItems(roadTripId) {
   const dbRoadTrip = await getRoadTrip(roadTripId);
   await RoadTrip.populate(dbRoadTrip, 'packedItems');
   return dbRoadTrip.packedItems;
 }
 
-export async function createPackedItems(roadTripId, packedItems) {
-  const dbPackedItems = new PackedItems(packedItems);
-  await dbPackedItems.save();
+export async function getPackedItemsForUser(roadTripId, userId) {
+  const allPackedItems = await getAllPackedItems(roadTripId);
+  return allPackedItems.find((packedItems) => new String(packedItems.user).valueOf() === new String(userId).valueOf());
+}
+
+export async function createPackedItemsForUser(roadTripId, userId, packedItems) {
+  packedItems.user = userId;
+
+  const dbPackedItemsForUser = new PackedItems(packedItems);
+  await dbPackedItemsForUser.save();
 
   const dbRoadTrip = await getRoadTrip(roadTripId);
-  dbRoadTrip.packedItems.push(dbPackedItems._id);
+  dbRoadTrip.packedItems.push(dbPackedItemsForUser._id);
   await dbRoadTrip.save();
 
-  return dbPackedItems;
+  return dbPackedItemsForUser;
+}
+
+export async function updatePackedItemsForUser(originalPackedItemsForUser, newPackedItemsForUser) {
+  originalPackedItemsForUser.items = newPackedItemsForUser.items;
+  await originalPackedItemsForUser.save();
+
+  return originalPackedItemsForUser;
 }
