99f826012825079e05565d7519e6a5e628f864d6
Courtney Hunter <courtney.hunter.nz@gmail.com>
2021-05-03 12:37:07 +1200

    Create Roadie Playlist

--- frontend/src/components/playlist/InitialisePlaylist/InitialisePlaylist.jsx
+++ frontend/src/components/playlist/InitialisePlaylist/InitialisePlaylist.jsx
@@ -1,5 +1,6 @@
 import { Button } from '@material-ui/core';
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 const SPOTIFY_BASE = 'https://accounts.spotify.com';
 const redirect_uri = 'http://localhost:3000/road-trip/608a11a9b1a2ffd010b1d738/spotify-playlist/callback';
@@ -8,35 +9,8 @@ const client_secret = process.env.REACT_APP_SPOTIFY_CLIENT_SECRET;
 
 const request = require('request'); // "Request" library
 
-const getToken = (authorization_code) => {
-  const authOptions = {
-    url: 'https://accounts.spotify.com/api/token',
-    form: {
-      code: authorization_code,
-      redirect_uri,
-      grant_type: 'authorization_code',
-    },
-    headers: {
-      Authorization: `Basic ${Buffer.from(`${client_id}:${client_secret}`).toString('base64')}`,
-    },
-    json: true,
-  };
-
-  request.post(authOptions, function (error, response, body) {
-    console.log(body);
-
-    if (!error && response.statusCode === 200) {
-      console.log('Success!');
-      console.log(response);
-    } else {
-      console.log('Bad!');
-      console.log(response);
-    }
-  });
-};
-
 function authorizeSpotify() {
-  const scopes = 'user-read-private user-read-email';
+  const scopes = 'user-read-private user-read-email playlist-modify-public playlist-modify-private';
   const auth_url =
     `${SPOTIFY_BASE}/authorize?` +
     `client_id=${client_id}` +
@@ -48,11 +22,79 @@ function authorizeSpotify() {
 }
 
 export default function InitialisePlaylist() {
+  const [accessToken, setAccessToken] = useState('');
+  const [refreshToken, setRefreshToken] = useState('');
+  const [userId, setUserId] = useState('');
+
+  const getToken = (authorization_code) => {
+    const authOptions = {
+      url: 'https://accounts.spotify.com/api/token',
+      form: {
+        code: authorization_code,
+        redirect_uri,
+        grant_type: 'authorization_code',
+      },
+      headers: {
+        Authorization: `Basic ${Buffer.from(`${client_id}:${client_secret}`).toString('base64')}`,
+      },
+      json: true,
+    };
+
+    request.post(authOptions, function (error, response, body) {
+      if (!error && response.statusCode === 200) {
+        setAccessToken(body?.access_token);
+        setRefreshToken(body?.refresh_token);
+
+        axios({
+          url: 'https://api.spotify.com/v1/me',
+          method: 'GET',
+          headers: {
+            Accept: 'application/json',
+            Authorization: `Bearer ${body?.access_token}`,
+            'Content-Type': 'application/json',
+          },
+        }).then((response) => {
+          setUserId(response.data.id);
+
+          axios({
+            url: `https://api.spotify.com/v1/users/${response.data.id}/playlists`,
+            method: 'POST',
+            headers: {
+              Accept: 'application/json',
+              Authorization: `Bearer ${body?.access_token}`,
+              'Content-Type': 'application/json',
+            },
+            data: {
+              name: 'collaborative one!',
+              description: 'This is my roadie playlist woo',
+              public: false,
+              collaborative: true,
+            },
+          }).then((response) => {
+            console.log('playlist created');
+            console.log(response);
+          });
+        });
+      }
+    });
+  };
+
+  function createPlaylist(authorization_code) {
+    getToken(authorization_code);
+  }
+
   const urlParams = new URLSearchParams(window.location.search);
   const authorization_code = urlParams.get('code');
 
   if (authorization_code) {
-    return <Button onClick={() => getToken(authorization_code)}>Get Token</Button>;
+    return (
+      <div>
+        <p>Access Token: {accessToken}</p>
+        <p>Refresh Token: {refreshToken}</p>
+        <p>User ID: {userId}</p>
+        <Button onClick={() => createPlaylist(authorization_code)}>Get Token</Button>
+      </div>
+    );
   }
 
   return <Button onClick={authorizeSpotify}>Log In to Spotify</Button>;
