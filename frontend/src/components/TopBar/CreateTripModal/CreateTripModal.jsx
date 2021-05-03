3a463b194dbeef69286f09fdc7d2fe5797e94f05
Denijar <denijar@gmail.com>
2021-05-03 17:05:32 +1200

    remove unused import

--- frontend/src/components/TopBar/CreateTripModal/CreateTripModal.jsx
+++ frontend/src/components/TopBar/CreateTripModal/CreateTripModal.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { useParams } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
