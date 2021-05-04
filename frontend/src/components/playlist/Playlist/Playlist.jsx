643f54ec871153e84c0ddf70ac81a22532dbdff3
Courtney Hunter <courtney.hunter.nz@gmail.com>
2021-05-04 23:52:37 +1200

    Show message for empty playlist

--- frontend/src/components/playlist/Playlist/Playlist.jsx
+++ frontend/src/components/playlist/Playlist/Playlist.jsx
@@ -16,14 +16,22 @@ export default function Playlist({ content }) {
             </TableRow>
           </TableHead>
           <TableBody>
-            {content.tracks.map((track) => (
-              <TableRow key={track.name} className={styles.tableRow}>
-                <TableCell component="th" scope="row">
-                  {track.name}
+            {content.tracks.length > 0 ? (
+              content.tracks.map((track) => (
+                <TableRow key={track.name} className={styles.tableRow}>
+                  <TableCell component="th" scope="row">
+                    {track.name}
+                  </TableCell>
+                  <TableCell align="right">{track.artists.toString()}</TableCell>
+                </TableRow>
+              ))
+            ) : (
+              <TableRow key="empty-playlist" className={styles.emptyPlaylist}>
+                <TableCell colSpan={2} align="center" component="th" scope="row">
+                  This playlist currently has no songs.
                 </TableCell>
-                <TableCell align="right">{track.artists.toString()}</TableCell>
               </TableRow>
-            ))}
+            )}
           </TableBody>
         </Table>
       </TableContainer>
