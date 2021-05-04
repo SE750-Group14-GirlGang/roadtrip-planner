36c88e5c45e8b0be1a621dd18cb918d940f25752
Denijar <denijar@gmail.com>
2021-05-05 10:30:52 +1200

    splits up createEmergencyDetails test

--- backend/src/db/controllers/__tests__/emergencydetails.test.js
+++ backend/src/db/controllers/__tests__/emergencydetails.test.js
@@ -1,7 +1,8 @@
 import * as emergencydetails from '../emergencydetails';
 import * as roadtrips from '../roadtrips';
-import * as EmergencyDetails from '../../models/EmergencyDetails';
-import * as RoadTrip from '../../models/RoadTrip';
+import * as EmergencyDetailsAll from '../../models/EmergencyDetails';
+import { EmergencyDetails } from '../../models/EmergencyDetails';
+import { RoadTrip } from '../../models/RoadTrip';
 
 jest.mock('../../models/EmergencyDetails');
 jest.mock('../../models/RoadTrip');
@@ -23,10 +24,21 @@ describe('getEmergencyDetails', () => {
 });
 
 describe('createEmergencyDetails', () => {
-  it('create emergency details object in database and saves in the emergencyDetails array roadtrip', async () => {
-    const roadTrip = { emergencyDetails: [432], save: () => {} };
+  it('creates emergency details object in database', async () => {
+    const roadTrip = { emergencyDetails: [432], save: jest.fn() };
     jest.spyOn(roadtrips, 'getRoadTrip').mockImplementation(() => Promise.resolve(roadTrip));
-    const spy = jest.spyOn(EmergencyDetails, 'EmergencyDetails').mockImplementation((emergencyDetails) => {
+
+    const emergencyDetails = { name: 'Denise' };
+    await emergencydetails.createEmergencyDetails(1, emergencyDetails);
+
+    expect(EmergencyDetails).toHaveBeenCalledTimes(1);
+    expect(EmergencyDetails).toHaveBeenCalledWith(emergencyDetails);
+  });
+
+  it('saves new emergency details object id in roadtrips emergency details array', async () => {
+    const roadTrip = { emergencyDetails: [432], save: jest.fn() };
+    jest.spyOn(roadtrips, 'getRoadTrip').mockImplementation(() => Promise.resolve(roadTrip));
+    const spy = jest.spyOn(EmergencyDetailsAll, 'EmergencyDetails').mockImplementation((emergencyDetails) => {
       return {
         _id: 123,
         name: emergencyDetails.name,
@@ -36,7 +48,6 @@ describe('createEmergencyDetails', () => {
 
     const result = await emergencydetails.createEmergencyDetails(1, { name: 'Denise' });
 
-    expect(spy).toHaveBeenCalledTimes(1);
     expect(result.name).toBe('Denise');
     expect(roadTrip.emergencyDetails).toStrictEqual([432, 123]);
   });
