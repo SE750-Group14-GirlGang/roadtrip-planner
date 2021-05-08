a9be71e0faf8e212607de6357c08a5dac0d3d7f1
Jennak21 <12jenna.k@gmail>
2021-05-08 17:56:46 +1200

    try to get backend deployed

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,22 +14,22 @@ const port = process.env.PORT || 3001;
 app.use(express.json());
 app.use('/', routes);
 
-// Make the "public" folder available statically
-app.use(express.static(path.join(__dirname, '../public')));
+// // Make the "public" folder available statically
+// app.use(express.static(path.join(__dirname, '../public')));
 
-// Serve up the frontend's "build" directory, if we're running in production mode.
-if (process.env.NODE_ENV === 'production') {
-  console.log('Running in production!');
+// // Serve up the frontend's "build" directory, if we're running in production mode.
+// if (process.env.NODE_ENV === 'production') {
+//   console.log('Running in production!');
 
-  // Make all files in that folder public
-  app.use(express.static(path.join(__dirname, '../../frontend/build')));
+//   // Make all files in that folder public
+//   app.use(express.static(path.join(__dirname, '../../frontend/build')));
 
-  // // If we get any GET request we can't process using one of the server routes, serve up
-  // // index.html by default.
-  // app.get('*', (req, res) => {
-  //   res.sendFile(path.join(__dirname, '../../frontend/build/index.html'));
-  // });
-}
+//   // If we get any GET request we can't process using one of the server routes, serve up
+//   // index.html by default.
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../../frontend/build/index.html'));
+//   });
+// }
 
 // Start the DB running. Then, once it's connected, start the server.
 connectToDatabase().then(async () => {
