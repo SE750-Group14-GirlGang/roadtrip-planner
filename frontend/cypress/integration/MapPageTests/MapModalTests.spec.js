1a45dd20c2a125ecc14bd7bae9272c5979f2c398
Laura Huijser <laura.huijser@yahoo.com>
2021-05-09 15:41:38 +1200

    Update MapModalTests.spec.js

--- frontend/cypress/integration/MapPageTests/MapModalTests.spec.js
+++ frontend/cypress/integration/MapPageTests/MapModalTests.spec.js
@@ -2,7 +2,28 @@
 
 context('Actions', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:3000/road-trip/60972c5df151d3ebb2a408b6/map');
+    cy.visit('http://localhost:3000/road-trip/1/map%27');
+    cy.intercept('GET', '/api/roadtrip/1', {
+      statusCode: 200,
+      body: {},
+    });
+    cy.intercept('GET', '/api/roadtrip/1/attendees', {
+      statusCode: 200,
+      body: ['a', 'b', 'c'],
+    });
+
+    cy.intercept('GET', '/api/roadtrip/1/map', {
+      statusCode: 200,
+      body: {},
+    });
+    cy.intercept('GET', '/api/roadtrip/1/isUserOrganiser', {
+      statusCode: 200,
+      body: { result: true },
+    });
+    cy.intercept('GET', '/api/roadtrip/1/organiser', {
+      statusCode: 200,
+      body: {},
+    });
   });
 
   // https://on.cypress.io/interacting-with-elements
